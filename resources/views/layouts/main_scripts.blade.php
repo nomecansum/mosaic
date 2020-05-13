6200cfac517707a75b49a22c1ffab31d0febc416
carlosloring <carlos.loring@gmail.com>
2020-05-13 18:42:19 +0200

    Revert "Revert "cambios""
    This reverts commit 2ab738fdea82421187e7b59ed839cf5c03a283ea.

--- resources/views/layouts/main_scripts.blade.php
+++ resources/views/layouts/main_scripts.blade.php
@@ -218,12 +218,19 @@
             }
         }).fail(function(r){
             //alert(JSON.parse(r.responseText)[0]);
-            //alert(r.responseJSON.errors.name[0]);
-            //console.log(r.responseJSON.errors.email);
+            //alert(r.responseJSON.errors.email[0]);
+            //console.log(r.responseJSON.errors.name[0]);
+
+            //console.log(r.responseJSON.errors.name[0]||r.responseJSON.errors.email[0]||r.responseJSON.errors.password[0]);
+
             //toast_error("Login",JSON.parse(r.responseText)[0]);
-            toast_error("Login",r.responseJSON.errors.password[0]);
+
+            //toast_error("Login",r.responseJSON.errors.name[0]||r.responseJSON.errors.email[0]||r.responseJSON.errors.password[0]);
+
             toast_error("Login",r.responseJSON.errors.name[0]);
             toast_error("Login",r.responseJSON.errors.email[0]);
+            toast_error("Login",r.responseJSON.errors.password[0]);
+
         })
         .always(function(){
             $('#spin_login').hide();

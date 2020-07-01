4a9eeae9875dc9735eb1e33cc5f31f7e9e197d47
carlosloring <carlos.loring@gmail.com>
2020-07-01 16:13:43 +0200

    Wizard antes de cambios en javascript

--- resources/views/users/import.blade.php
+++ resources/views/users/import.blade.php
@@ -87,75 +87,87 @@
                                     <!--form-->
                                     <form name="form_fichero" id="form_fichero"  enctype="multipart/form-data"  action="{{ url('users/import/process_import') }}" class="tab-wizard wizard-circle form-horizontal" method="POST">
 
+                                        <input type="hidden" name="fichero" id="fic">
+
+                                        @csrf
 
 					                    <div class="panel-body">
 					                        <div class="tab-content">
 
 					                            <!--First tab-->
 					                            <div id="demo-main-tab1" class="tab-pane">
+
                                                     <h4>Descarge la plantilla EXCEL con los datos especificos de su empresa.</h4>
-                                                    <div class="text-center">
-                                                        <a class="link_excel hover-this" href="javascript:void(0)" id="link_descarga" @if(isAdmin() || count(clientes())>1)style="display: none"@endif>
-                                                            <img src="{{ url('imgs/logo_excel.png') }}">
-                                                            <span><h2 id="nombre_fichero" style="color: #007233">
-                                                                @if(!isAdmin() && count(clientes()) == 1)
-                                                                plantilla_cucoweb_{{ \DB::table('cug_clientes')->where('cod_cliente',Auth::user()->cod_cliente)->first()->nom_cliente }}_{{ Carbon\Carbon::today()->format('Ymd')}}.xlsx
-                                                                @endif
-                                                            </h2></span>
-                                                        </a>
-                                                    </div>
-                                                    <br><br>
-                                                    <h4>Recuerde que si altera la parametrización de su empresa (Departamentos/Centros/Horarios/Ciclos) deberá volver a descargarla para asegurar que tiene los datos actualizados</h4>
-                                                    <br>
+                                                        @if(isAdmin() || count(clientes())>1)
+                                                            Seleccione la empresa<br><br>
+                                                            <select required class="" id="id_cliente" name="id_cliente" palceholder="Seleccione la empresa">
+                                                                <option selected value=""></option>
+                                                                @foreach (@\DB::table('clientes')->orderby('nom_cliente')->get() as $c_)
+                                                                    <option value="{{$c_->id_cliente}}">{{$c_->nom_cliente}}</option>
+                                                                @endforeach
+                                                            </select>
+
+                                                        @else
+                                                            <input type="hidden" id="id_cliente" name="id_cliente" value="{{ Auth::user()->id_cliente }}"
+                                                        @endif
+
+                                                        <br>
+                                                        <div class="text-center">
+                                                            <a class="link_excel hover-this" href="javascript:void(0)" id="link_descarga" @if(isAdmin() || count(clientes())>1)style="display: none"@endif>
+                                                                <img src="{{ url('imgs/logo_excel.png') }}">
+                                                                <span><h2 id="nombre_fichero" style="color: #007233">
+                                                                    @if(!isAdmin() && count(clientes()) == 1)
+                                                                    plantilla_cucoweb_{{ \DB::table('clientes')->where('id_cliente',Auth::user()->id_cliente)->first()->nom_cliente }}_{{ Carbon\Carbon::today()->format('Ymd')}}.xlsx
+                                                                    @endif
+                                                                </h2></span>
+                                                            </a>
+                                                        </div>
+                                                        <br><br>
+                                                        <h4>Recuerde que si altera la parametrización de su empresa (Departamentos/Centros/Horarios/Ciclos) deberá volver a descargarla para asegurar que tiene los datos actualizados</h4>
+                                                        <br>
 
 					                            </div>
 
 					                            <!--Second tab-->
 					                            <div id="demo-main-tab2" class="tab-pane fade">
 
+                                                    <h4>Rellene la plantilla EXCEL con los datos de sus empleados.</h4>
 
-                        <section>
-                                <h4>Rellene la plantilla EXCEL con los datos de sus empleados.</h4>
-
-                                <h4>Hay ciertos campos que son obligatorios, debe tener en cuenta de que si alguno de dichos campos no estan rellenos, se rechazará el la plantilla durante el procesado.</h4>
-                                <h4 class="font-bold">Campos obligatorios</h4>
-                                <div class="row">
-                                    <div class="col-md-3">
-                                        <ul>
-                                            <li>Nombre</li>
-                                            <li>Apeliidos</li>
-                                            <li>Situacion</li>
-                                            <li>e-mail</li>
-                                        </ul>
-                                    </div>
-                                    <div class="col-md-3">
-                                        <ul>
-                                            <li>Estado civil</li>
-                                            <li>Sexo</li>
-                                            <li>Fecha de nacimiento</li>
-                                            <li>Nivel de estudios</li>
-                                        </ul>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <img src="{{ url('imgs/ejemplo_excel.png') }}">
-                                    </div>
-                                </div>
-                                <h4>Para estos dos campos, si no indica valor, se asignará el primero disponible por defecto</h4>
-                                <ul>
-                                    <li>Centro</li>
-                                    <li>Departamento</li>
-                                </ul>
-                                <h4>Una vez rellenada la plantilla pulse "Siguiente"</h4>
-                        </section>
-
-
+                                                    <h4>Hay ciertos campos que son obligatorios, debe tener en cuenta de que si alguno de dichos campos no estan rellenos, se rechazará el la plantilla durante el procesado.</h4>
+                                                    <h4 class="font-bold">Campos obligatorios</h4>
+                                                    <div class="row">
+                                                        <div class="col-md-3">
+                                                            <ul>
+                                                                <li>Nombre</li>
+                                                                <li>Apeliidos</li>
+                                                                <li>Situacion</li>
+                                                                <li>e-mail</li>
+                                                            </ul>
+                                                        </div>
+                                                        <div class="col-md-3">
+                                                            <ul>
+                                                                <li>Estado civil</li>
+                                                                <li>Sexo</li>
+                                                                <li>Fecha de nacimiento</li>
+                                                                <li>Nivel de estudios</li>
+                                                            </ul>
+                                                        </div>
+                                                        <div class="col-md-6">
+                                                            <img src="{{ url('imgs/ejemplo_excel.png') }}">
+                                                        </div>
+                                                    </div>
+                                                    <h4>Para estos dos campos, si no indica valor, se asignará el primero disponible por defecto</h4>
+                                                    <ul>
+                                                        <li>Centro</li>
+                                                        <li>Departamento</li>
+                                                    </ul>
+                                                    <h4>Una vez rellenada la plantilla pulse "Siguiente"</h4>
 
 					                            </div>
 
 					                            <!--Third tab-->
 					                            <div id="demo-main-tab3" class="tab-pane">
 
-                                                    <section>
                                                         <h4>Ahora arrastre los ficheros que desea subir: imagenes de empleados y la plantilla de excel rellenada.<br><br></h4>
                                                         <i class="fa fas fa-info-circle"></i>NOTA: Las fotos de empleados deben tener el mismo nombre que haya puesto en la plantilla, si no, se descartarán.<br><br>
                                                         <h4>Es importante que <b>suba la plantilla en ultimo lugar</b>, pues al subir la plantilla se iniciará el procesado de la informacion y una vez subida ya no se podrán subir más ficheros con fotos.</h4>
@@ -174,8 +186,6 @@
                                                                 <h2><i class="mdi mdi-cloud-upload"></i> Arrastre archivos <span class="text-blue">para subirlos</span></h2>&nbsp&nbsp<h6 class="display-inline text-muted"> (o Click aqui)</h6>
                                                             </div>
                                                         </div>
-                                                    </section>
-
 					                            </div>
 
 					                            <!--Fourth tab-->
@@ -215,7 +225,11 @@
 </div>
 @endsection
 
+@section('scripts')
 
+<script type="text/javascript" src="{{url('/plugins/dropzone/dropzone.js')}}" ></script>
+
+@endsection
 
 
 
